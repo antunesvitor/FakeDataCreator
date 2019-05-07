5b24d00b316175a92e40ea113fbb6bf77ce4f21d
antunesvitor <vguilhermeantunes@gmail.com>
2019-05-07 17:32:53 -0300

    Adcionado classe data que tinha sido removido no commit anterior

--- DataMockers/PolynomialMocker.cs
+++ DataMockers/PolynomialMocker.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using MockingData.Models;
 
 namespace MockingData.DataMockers
 {
---
+++ Models/Data.cs
@@ -0,0 +1,15 @@
+namespace MockingData.Models
+{
+    
+    public class data
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public  data(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
